786c52bc9a60e5dd48e22fcb0ae6d015102af3d6
Manuel <mgamez244@gmail.com>
2023-05-30 16:05:04 +0100

    feat(personaje) creada la clase personaje

---
+++ src/com/salesianos/personaje/Personaje.java
@@ -0,0 +1,39 @@
+package com.salesianos.personaje;
+
+
+public class Personaje {
+    private int salud;
+    private int defensa;
+    private int fuerza;
+    
+    public Personaje(int defensa, int fuerza) {
+        this.salud = 12; 
+        this.defensa = defensa;
+        this.fuerza = fuerza;
+    }
+    
+    
+    public int getSalud() {
+        return salud;
+    }
+    
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+    
+    public int getDefensa() {
+        return defensa;
+    }
+    
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+    
+    public int getFuerza() {
+        return fuerza;
+    }
+    
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+}
